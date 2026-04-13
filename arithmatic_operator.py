f0800318e0ebbd367d6f828f139c35e3dfad2924
nikhilengg <tedlapunikhil@gmail.com>
2026-04-13 23:42:48 +0530

    Day 5: Practice programs (typecasting and basics)

---
+++ arithmatic_operator.py
@@ -0,0 +1,6 @@
+print(7+2)
+print(5/2)
+print(4//2)#floor division
+print(2**4)
+print(5%2)
+print(5+2*3-1+10/5)
---
+++ bmi.py
@@ -0,0 +1,4 @@
+weight=int(input("enter weight: "))
+height=float(input("enter height: "))
+bmi=weight/(height**height)
+print(bmi)
