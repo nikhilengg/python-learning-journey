c29894e99749f4368d98bf55900c99eab0c870ae
nikhilengg <tedlapunikhil@gmail.com>
2026-04-19 21:51:31 +0530

    day 10: python practise

---
+++ f_string.py
@@ -0,0 +1,6 @@
+name='nikhil'
+age=18
+height=1.6
+#print("my name is"+name,"i am"+str(age),"years old")
+#print("my name is" ,name, "i am   ", age,"years old")
+print(f"my name is {name} iam {age} years old my height is {height} meters")
---
+++ if_else.py
@@ -0,0 +1,5 @@
+height=int(input("enter height in feet: "))
+if(height>3):
+    print("buy ticket")
+else:
+    print("no ticket required")
